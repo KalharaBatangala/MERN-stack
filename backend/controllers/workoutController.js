6f21720f191061423f0a1208710ab3ec6f446969
Kalhara Batangala <kalharabatangala@gmail.com>
2024-08-15 06:26:42 +0530

    Routes for update and delete workout

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,4 +1,6 @@
 const Workout = require('../models/workoutModel')
+const mongoose = require('mongoose')
+
 
 //get all workouts
 const getWorkouts = async (req, res) => {
@@ -11,6 +13,11 @@ const getWorkouts = async (req, res) => {
 
 const getWorkout = async (req, res) => {
     const {id } = req.params
+    
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'No such workout'})
+    }
+    
     const workout = await Workout.findById(id)
 
     if(!workout) {
@@ -37,13 +44,46 @@ const createWorkout = async(req, res) => {
 
 
 //delete a workout
+const deleteWorkout = async(req, res) => {
+    const {id} = req.params
 
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'No such workout'})
+    }
+    const workout = await Workout.findOneAndDelete({_id: id})
+    if(!workout) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    res.status(200).json(workout)
+}
 
 
 //update a workout
+const updateWorkout = async(req, res) => {
+    const {id} = req.params
 
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'No such workout'})
+    }
+    const workout = await Workout.findOneAndUpdate({_id: id}, {
+        ...req.body
+    })
+
+    if(!workout) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    res.status(200).json(workout)
+
+}
 
 
 module.exports = {
-    createWorkout
+    createWorkout,
+    getWorkout,
+    getWorkouts,
+    deleteWorkout,
+    updateWorkout
+
 }
