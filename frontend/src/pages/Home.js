b59c88c7a5d1c189606af56e044bdf31d32bdda8
Kalhara Batangala <kalharabatangala@gmail.com>
2024-08-19 07:02:21 +0530

    Updated home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,28 @@
+import {useEffect, useState} from 'react'
+
+
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null)
+    //useEffect hook
+    useEffect( () => {
+        const fetchWorkouts = async () => {
+            const response = await('/api/workouts')
+            const json = await response.json()
+
+            if (response.ok){
+                setWorkouts(json)
+            }
+        }
+    }, [])   // only call once
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className='workouts'>
+                {workouts && workouts.map( (workout) => (
+                    <p key={workout._id}>{workout.title}</p>
+                ))}
+
+            </div>
 
         </div>
     )
