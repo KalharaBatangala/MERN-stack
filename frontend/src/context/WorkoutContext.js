ab84daeca3b4638e11bd5e9a5cdcc0a39750b4b8
Kalhara Batangala <kalharabatangala@gmail.com>
2024-09-01 15:12:22 +0530

    Updated workoutContext.js

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -1,12 +1,34 @@
-import {createContext} from 'react'
+import {createContext, useReducer} from 'react'
 
 
 export const WorkoutsContext = createContext()
 
+export const workoutsReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_WORKOUTS':
+            return {
+                workouts: action.payload
+            }
+        case 'CREATE_WORKOUT':
+            return {
+                workouts: [action.payload, ...state.workouts]
+            }
+    }
+}
+
+
+
+
 export const WorkoutsContextProvider = ({children}) => {
 
+    const [state, dispatch] = useReducer(workoutsReducer, {
+        workout:null
+    })
+    
+    
+
     return (
-        <WorkoutsContext.Provider>
+        <WorkoutsContext.Provider value={}>
             {children}
         </WorkoutsContext.Provider>
     )
