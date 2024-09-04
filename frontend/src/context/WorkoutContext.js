6e073e9052cdbd499d65e48275fde05e809c6af9
Kalhara Batangala <kalharabatangala@gmail.com>
2024-09-04 08:44:07 +0530

    Updated Home.js, added dispatch

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -30,7 +30,7 @@ export const WorkoutsContextProvider = ({children}) => {
     
 
     return (
-        <WorkoutsContext.Provider value={{state, dispatch}} >
+        <WorkoutsContext.Provider value={{...state, dispatch}} >
             {children}
         </WorkoutsContext.Provider>
     )
