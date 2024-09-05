2fbd3991718aad3d876100d02bf968bff30a2771
Kalhara Batangala <kalharabatangala@gmail.com>
2024-09-05 09:07:42 +0530

    Handling errors

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -7,7 +7,8 @@ const WorkoutForm = () => {
     const [load, setLoad] = useState('')
     const [reps, setReps] = useState('')
     const [error, setError] = useState(null)
-    
+    const [emptyFields, setEmptyFields] = useState([])
+
     const handleSubmit = async(e) => {
         e.preventDefault()  //default is refresh the page. don't want to happen it
 
@@ -26,12 +27,14 @@ const WorkoutForm = () => {
         //check for error
         if(!response.ok) {
             setError(json.error)
+            setEmptyFields(json.emptyFields)
         }
         if(response.ok) {
             setTitle('')
             setLoad('')
             setReps('')
             setError(null)
+            setEmptyFields([])
             console.log('New Workout added', json)
             dispatch({type:'CREATE_WORKOUT', payload: json})
         }
@@ -43,7 +46,7 @@ const WorkoutForm = () => {
             <h3>Add a New Workout</h3>
 
             <label>Exercise Title :</label>
-            <input type='text' onChange={(e) => setTitle(e.target.value)} value={title} />
+            <input type='text' onChange={(e) => setTitle(e.target.value)} value={title} className={emptyFields.includes('title'?)} />
             <label>Load (in kg) :</label>
             <input type='number' onChange={(e) => setLoad(e.target.value)} value={load} />
             <label>Reps :</label>
